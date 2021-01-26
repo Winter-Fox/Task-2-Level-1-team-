44f46a46c752d90cf89522de2b6f97ce11a58313
Winter Fox <56193688+Winter-Fox@users.noreply.github.com>
2021-01-26 14:10:45 +0200

    Update design.py

--- design.py
+++ design.py
@@ -70,7 +70,7 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.textBrowser.setText("Вибрати Стандартну базу даних.")
+        self.textBrowser.setText("Обрати Стандартну базу даних.")
         self.textBrowser_2.setText("Обрати свою.")
         self.textBrowser_3.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"-1\" /><style type=\"text/css\">\n"
